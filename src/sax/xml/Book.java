868af49fcd8d58d1a40cbb45ed072f9f895ee772
Formação <Formao@PCVant005.lan>
2015-05-21 10:52:25 +0100

    Construtor de SaXXML

---
+++ src/sax/xml/Book.java
@@ -0,0 +1,93 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sax.xml;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+/**
+ *
+ * @author Formação
+ */
+public class Book {
+    String lang;
+    String title;
+    String id;
+    String isbn;
+    Date regDate;
+    String publisher;
+    int price;
+    List<String> authors;
+        public Book(){
+            authors = new ArrayList<>();
+        }
+
+    public String getLang() {
+        return lang;
+    }
+
+    public void setLang(String lang) {
+        this.lang = lang;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public Date getRegDate() {
+        return regDate;
+    }
+
+    public void setRegDate(Date regDate) {
+        this.regDate = regDate;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public List<String> getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(List<String> authors) {
+        this.authors = authors;
+    }
+       
+}
--- src/sax/xml/SaxXml.java
+++ src/sax/xml/SaxXml.java
@@ -5,17 +5,43 @@
  */
 package sax.xml;
 
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
+import jdk.internal.org.xml.sax.helpers.DefaultHandler;
+
 /**
  *
  * @author Formação
  */
-public class SaxXml {
+public class SaxXml extends DefaultHandler {
 
+        List<Book> bookL;
+        String bookXmlFileName;
+        String tmpValue;
+        Book bookTmp;
+        SimpleDateFormat sdf =
+                new SimpleDateFormat("yyyy-MM-dd");
+        
+        public SaxXml(String bookXmlFilename) {
+            this.bookXmlFileName = bookXmlFilename;
+            bookL = new ArrayList<>();
+            parseDocumentSAX();
+            printDataSax();
+        }
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+    }
+
+    private void parseDocumentSAX() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private void printDataSax() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
 }
